30b8f431aca93d09706d53a67eab49e2fbcd9c98
softengineerstevens <softengineerstevens@gmail.com>
2024-06-29 10:25:18 +0300

    variable declaration

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+
+const currentUser = 'Steve';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = (welcomeMessage.toLocaleUpperCase()); 
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
